332e63135b8f24c42f8d845aae2381be19d15b18
Peter Clarke <clarke.peterf@gmail.com>
2025-03-11 14:55:15 -0400

    - Fix gulpfile to use proper ESM imports/exports - Update ml-proxy to handle importing from generated CommonJS - Have gulpfile give generated file a .cjs extension (MarkLogic generates common JS, previous module export was a manual change to the generated file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 // gulp config for generating code from MarkLogic data service
 
 import gulp from 'gulp'
-import proxy from 'marklogic/lib/proxy-generator'
+import { generate } from 'marklogic/lib/proxy-generator.js'
+import rename from 'gulp-rename'
 
 function proxygen() {
   return gulp
     .src('../lux-marklogic/src/main/ml-modules/root/ds/*')
-    .pipe(proxy.generate())
+    .pipe(generate())
+    .pipe(rename({ extname: '.cjs' }))
     .pipe(gulp.dest('lib/ml-generated'))
 }
 
-export default proxygen
+export { proxygen }
