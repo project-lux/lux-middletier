c34aaa285015ec90ccdd6bcaf66df2836a1387ff
Seong-June Kim <seong-june.kim@yale.edu>
2025-06-09 16:24:24 -0400

    Add HEAD and PATCH to allowed methods

--- app/app.js
+++ app/app.js
@@ -74,7 +74,7 @@ class App {
 
     exp.options('*', (req, res) => {
       res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+      res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS')
       res.sendStatus(200)
     })
 
