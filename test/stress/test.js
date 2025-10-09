33c3c2ef540b1b8727c546b8a82b05580b2fd183
Peter Clarke <clarke.peterf@gmail.com>
2025-10-09 08:45:24 -0400

    run against dev - no caching; add some more api endpoints; decrease request rate; console.log before making http request so that you can see the order in which iterations start, not when they finish

--- test/stress/test.js
+++ test/stress/test.js
@@ -5,12 +5,16 @@ import exec from 'k6/execution';
 const data = new SharedArray('some name', function () {
   // All heavy work (opening and processing big files for example) should be done inside here.
   // This way it will happen only once and the result will be shared between all VUs, saving time and memory.
-  
+
   // this can read from a file later
   const f = [
-    "https://lux-front-tst.collections.yale.edu/api/search/item?q=%7B%22AND%22%3A%5B%7B%22text%22%3A%22andy%22%2C%22_lang%22%3A%22en%22%7D%2C%7B%22text%22%3A%22warhol%22%2C%22_lang%22%3A%22en%22%7D%5D%7D&page=1",
-    "https://lux-front-tst.collections.yale.edu/data/object/64a6f0c2-6d58-44d8-8ffe-43ff5d213b62",
-    "https://lux-front-tst.collections.yale.edu/data/person/66049111-383e-4526-9632-2e9b6b6302dd"
+    "https://lux-front-dev.collections.yale.edu/api/search/item?q=%7B%22AND%22%3A%5B%7B%22text%22%3A%22andy%22%2C%22_lang%22%3A%22en%22%7D%2C%7B%22text%22%3A%22warhol%22%2C%22_lang%22%3A%22en%22%7D%5D%7D&page=1",
+    "https://lux-front-dev.collections.yale.edu/data/object/64a6f0c2-6d58-44d8-8ffe-43ff5d213b62",
+    "https://lux-front-dev.collections.yale.edu/data/person/66049111-383e-4526-9632-2e9b6b6302dd",
+    "https://lux-front-dev.collections.yale.edu/api/facets/item?q=%7B%22AND%22%3A%5B%7B%22text%22%3A%22blue%22%2C%22_lang%22%3A%22en%22%7D%2C%7B%22text%22%3A%22whale%22%2C%22_lang%22%3A%22en%22%7D%5D%7D&name=itemHasDigitalImage&page=1",
+    "https://lux-front-dev.collections.yale.edu/api/facets/item?q=%7B%22AND%22%3A%5B%7B%22text%22%3A%22blue%22%2C%22_lang%22%3A%22en%22%7D%2C%7B%22text%22%3A%22whale%22%2C%22_lang%22%3A%22en%22%7D%5D%7D&name=itemEncounteredPlaceId&page=1",
+    "https://lux-front-dev.collections.yale.edu/api/related-list/agent?&name=relatedToAgent&uri=https%3A%2F%2Flux.collections.yale.edu%2Fdata%2Fperson%2F66049111-383e-4526-9632-2e9b6b6302dd"
+
 ]
   return f; // f must be an array
 });
@@ -25,7 +29,7 @@ export const options = {
       duration: '30s',
 
       // How many iterations per timeUnit
-      rate: 30,
+      rate: 20,
 
       // Start `rate` iterations per second
       timeUnit: '1s',
@@ -36,11 +40,12 @@ export const options = {
       // Spin up a maximum of 50 VUs to sustain the defined
       // constant arrival rate.
       maxVUs: 10,
+      gracefulStop: '1200s'
     },
   },
 };
 
 export default function() {
-  let res = http.get(data[(exec.scenario.iterationInTest)  % data.length]);
   console.log('VU:', __VU, 'iteration:', exec.scenario.iterationInTest, "data entry:", data[(exec.scenario.iterationInTest)  % data.length]);
+  let res = http.get(data[(exec.scenario.iterationInTest)  % data.length]);
 }
